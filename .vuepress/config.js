e4edca0dd41701f714307aa56433a9cc23cb956b
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2019-11-18 21:53:54 -0500

    Customize header and home README

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'The Compendium',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Jounal', link: '/journal/' },
+      { text: 'D&D Beyond', link: 'https://www.dndbeyond.com' }
+    ],
+  },
 }
