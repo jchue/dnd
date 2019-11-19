756edac90b26911b523e0e9b02f4cb3931f822fb
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2019-11-18 21:55:25 -0500

    Add first journal page and sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,10 +2,16 @@ module.exports = {
   title: 'The Compendium',
   description: 'Just playing around',
   themeConfig: {
+    displayAllHeaders: true,
+    lastUpdated: 'Last Updated',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Jounal', link: '/journal/' },
       { text: 'D&D Beyond', link: 'https://www.dndbeyond.com' }
+    ],
+    sidebar: [
+      ['/journal/', 'Journal'],
+      '/journal/2-10',
     ],
   },
 }
